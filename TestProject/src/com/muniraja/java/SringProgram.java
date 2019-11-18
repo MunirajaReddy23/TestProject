b7105fae58557fadc7be0b7361e49c419128be03
munirajareddy23 <bmuniraja.res23@gmail.com>
2019-11-18 21:06:00 +0530

    removed some lines in stringprogram.java class

--- TestProject/src/com/muniraja/java/SringProgram.java
+++ TestProject/src/com/muniraja/java/SringProgram.java
@@ -5,10 +5,8 @@ public class SringProgram {
 	public static void main(String[] args) {
 		String s1="hello";
 		String s2="java";
-		String s=" ";
 		String s3=new String("hello");
 		//if u compare sting=null with other string then it throws a null pointer exception
-		System.out.println(s.equals(s2));
 		System.out.println(s1==s2);
 		System.out.println(s1==s3);
 		System.out.println(s2==s3);
