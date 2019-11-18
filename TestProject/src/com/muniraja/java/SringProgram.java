ec6f6bc565ff445b888016968aaff8d3819913a0
munirajareddy23 <bmuniraja.res23@gmail.com>
2019-11-18 20:59:29 +0530

    removed s3.equals(s2) print statement

--- TestProject/src/com/muniraja/java/SringProgram.java
+++ TestProject/src/com/muniraja/java/SringProgram.java
@@ -6,14 +6,15 @@ public class SringProgram {
 		String s1="hello";
 		String s2="java";
 		String s=" ";
-		System.out.println(s.equals(s2));
 		String s3=new String("hello");
+		//if u compare sting=null with other string then it throws a null pointer exception
+		System.out.println(s.equals(s2));
 		System.out.println(s1==s2);
 		System.out.println(s1==s3);
 		System.out.println(s2==s3);
 		System.out.println(s1.equals(s2));
 		System.out.println(s1.equals(s3));
-		System.out.println(s3.equals(s2));
+		
 	}
 
 }
